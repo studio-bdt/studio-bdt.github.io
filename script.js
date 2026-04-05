fe985e8b7f205cc1d049212b2f7642ca32db839a
ImmatureGoat <63555728+octaviustheking@users.noreply.github.com>
2026-04-05 12:55:49 -0400

    blah blah balh

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const checkbox = document.getElementById('hotdog-checkbox');
+const hero = document.getElementById('top');
+const highlight = document.querySelector('.highlight');
+const cards = document.querySelectorAll('.card');
+
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('hotdog');
+    hero.classList.toggle('hotdog');
+    highlight.classList.toggle('hotdog');
+
+    cards.forEach(card => {
+        card.classList.toggle('hotdog');
+    });
 });
